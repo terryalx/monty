896da3aa6551957531aa2e4ff594fa2803c682aa
marwasabon <marwasabon@gmail.com>
2023-09-20 09:01:13 +0000

    merge

--- marwa_test/monty.h
+++ marwa_test/monty.h
@@ -10,14 +10,10 @@
 #include <ctype.h>
 #define _GNU_SOURCE
 
+extern int value;
+
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * struct stack_s - doubly linked list [stack]
  */
 typedef struct stack_s
 {
@@ -28,24 +24,25 @@ typedef struct stack_s
 
 /**
  * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-/** our functions **/
+
+/* error messages */
+void usage_error(void);
+void open_error(char *file);
+void push_error(FILE *fd, char *line, stack_t *stack, int line_number);
+void instr_error(FILE *fd, char *line, stack_t *stack, char *val, int line_n);
+
+/* functions */
 char **tokenizer(char *buffer);
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <string.h>
-
-
+void pop(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+stack_t *createNode(int n);
 
 #endif /* MONTY */
