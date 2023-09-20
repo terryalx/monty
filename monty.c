d06013dc0b0575972b34d897f7b5929f40eb94f6
setalxswe <setalxswe@gmail.com>
2023-09-19 20:14:10 -0700

    __monty__

--- monty.c
+++ monty.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: number of argument
+ * Return: success || failure
+ */
+
 int main(int argc, char *argv[])
 {
 	/* Check count */
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n" );
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* file path to open */
+	char *filepath = argv[1];
+	/* open file */
+	FILE *filetoopen = fopen();
+
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", file);
 		exit(EXIT_FAILURE);
 	}
 
---
+++ monty.h
@@ -0,0 +1,13 @@
+#ifndef _MONTY_H_
+#define _MONTY_H_
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+
+
+#endif /* MONTY */
