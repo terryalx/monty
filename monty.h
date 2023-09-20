bc91601b48961e81168999987c0785d77be90a41
setalxswe <setalxswe@gmail.com>
2023-09-20 01:25:17 -0700

    __func__

--- monty.h
+++ monty.h
@@ -8,6 +8,17 @@
 #include <stddef.h>
 #include <string.h>
 
+/* error messages */
+void usage_error(void);
+void open_error(char *file);
+void push_error(FILE *fd, char *line, stack_t *stack, int line_number);
+void instr_error(FILE *fd, char *line, stack_t *stack, char *val, int line_n);
 
+/* functions */
+void pop(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+stack_t *createNode(int n);
 
 #endif /* MONTY */
