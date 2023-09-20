d7a9461a3d56a6a750c7f08f61268484cb3076a9
_terry <setalxswe@gmail.com>
2023-09-20 13:03:27 +0100

    __main__

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * main - Entry point for Monty interpreter.
+ * @argc: Argument counter.
+ * @argv: Argument vector containing the filename.
+ * Return: EXIT_SUCCESS if successful, EXIT_FAILURE on failure.
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		/**
+		 * usage_error - Print usage error message.
+		 */
+		usage_error();
+	}
+	else
+	{
+		/**
+		 * execute - Read the Monty bytecode file and execute the commands.
+		 * @argv: The filename containing Monty bytecode.
+		 */
+		execute(argv[1]);
+	}
+
+	return (EXIT_SUCCESS);
+}
