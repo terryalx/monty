bb1afe459a68842ec6288afff89909ac51975b60
_terry <setalxswe@gmail.com>
2023-09-20 12:46:35 +0100

    __list__

---
+++ _mul.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * mul - multiplies the second top element of the stack with
+ * the top element of the stack.
+ * @stack: double pointer to the head of the stack
+ * @line_number: the line number
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+
+	stack_t *val = NULL;
+	int sum = 0;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+	val = (*stack)->next;
+	sum = val->n;
+	sum *= (*stack)->n;
+	pop(stack, line_number);
+	val->n = sum;
+}
