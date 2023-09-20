e349213abebaded828b12e28fb60d696ede51c23
_terry <setalxswe@gmail.com>
2023-09-20 12:37:00 +0100

    __pop__

---
+++ _nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - function
+ * @stack: double pointer to the head of the list
+ * @line_number: the current line number
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
